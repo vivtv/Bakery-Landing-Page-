10fca0fcc3086688e4b40220fedc045973c6afe9
xvivix07 <vtv.villalobos@gmail.com>
2025-01-09 18:57:45 +0700

    Configure Git LFS for large files

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.navbar ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
